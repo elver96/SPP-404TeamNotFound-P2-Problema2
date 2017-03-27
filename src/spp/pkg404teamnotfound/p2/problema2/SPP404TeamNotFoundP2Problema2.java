c43de6df6309b347c59d81811ffd3dccdbb3fc7b
elver <elver@192.168.1.75>
2017-03-26 19:47:29 -0600

    Proyecto 2 problema 2

--- src/spp/pkg404teamnotfound/p2/problema2/SPP404TeamNotFoundP2Problema2.java
+++ src/spp/pkg404teamnotfound/p2/problema2/SPP404TeamNotFoundP2Problema2.java
@@ -17,53 +17,49 @@ public class SPP404TeamNotFoundP2Problema2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int arreglo1 [][];
-        int numeroFilas1;
-        int numeroColumnas1;
+       int arreglo1[][];
+        int numFilas1;
+        int numColumnas1;
         int datoCasilla1;
-        System.out.println("Ingrese el numero de filas que tendra la matris #01:");
-        Scanner entradaNumeroFilas1 = new Scanner(System.in);
-        numeroFilas1 = entradaNumeroFilas1.nextInt();
-        System.out.println("Ingrese el numero de columnas que tendra la matris #01:");
-        Scanner entradaNumeroColumnas1 = new Scanner(System.in);
-        numeroColumnas1 = entradaNumeroColumnas1.nextInt();
-        arreglo1 = new int [numeroFilas1][numeroColumnas1];
-        for(int i = 0; i < arreglo1.length; i++){
-            for (int j = 0; j < arreglo1.length; j++){
-                System.out.println("Ingrese el dato que desee guardar en esta casilla:");
-                Scanner entradaCasilla = new Scanner(System.in);
-                datoCasilla1 = entradaCasilla.nextInt();
-                arreglo1 [i][j] = datoCasilla1;
+        int cont=0;
+        System.out.println("Matriz A");
+        numFilas1 = darTamaño(" filas:");
+        numColumnas1 = darTamaño(" columnas:");
+
+        arreglo1 = new int[numFilas1][numColumnas1];
+        for (int i = 0; i < arreglo1.length; i++) {
+            for (int j = 0; j < arreglo1.length; j++) {
+                cont++;
+                System.out.println("Ingrese el dato que desee guardar en la casilla: "+cont);
+                datoCasilla1 = solicitaDatos();
+                arreglo1[i][j] = datoCasilla1;
             }
         }
-        int arreglo2 [][];
-        int numeroFilas2;
-        int numeroColumnas2;
+        int arreglo2[][];
+        int numFilas2;
+        int numColumnas2;
         int datoCasilla2;
-        System.out.println("Ingrese el numero de filas que tendra la matris #02:");
-        Scanner entradaNumeroFilas2 = new Scanner(System.in);
-        numeroFilas2 = entradaNumeroFilas2.nextInt();
-        System.out.println("Ingrese el numero de columnas que tendra la matris #02:");
-        Scanner entradaNumeroColumnas2 = new Scanner(System.in);
-        numeroColumnas2 = entradaNumeroColumnas2.nextInt();
-        arreglo2 = new int [numeroFilas2][numeroColumnas2];
-        for(int i = 0; i < arreglo2.length; i++){
-            for(int j = 0; j < arreglo2[i].length; j++){
+        System.out.println("Matriz B");
+        numFilas2 = darTamaño(" filas:");
+        numColumnas2 = darTamaño(" columnas:");
+
+        arreglo2 = new int[numFilas2][numColumnas2];
+        for (int i = 0; i < arreglo2.length; i++) {
+            for (int j = 0; j < arreglo2[i].length; j++) {
                 System.out.println("Ingrese el dato que se guardara en esta casilla:");
-                Scanner entradaCasilla2 = new Scanner(System.in);
-                datoCasilla2 = entradaCasilla2.nextInt();
-                arreglo2 [i][j] = datoCasilla2;
+                datoCasilla2 = solicitaDatos();
+                arreglo2[i][j] = datoCasilla2;
             }
         }
-        if ((numeroFilas1 != numeroFilas2) || (numeroColumnas1 != numeroColumnas2)){
+        if ((numFilas1 != numFilas2) || (numColumnas1 != numColumnas2)) {
             System.out.println("LAS DIMENSONES DE AMBAS MATRICES NO SON IGUALES");
             System.exit(0);
         } else {
             System.out.println("LAS DIMENSIONES DE AMBAS MATRICES SON IGUALES");
         }
-        for(int i = 0; i < arreglo2.length; i++){
-            for(int j = 0; j < arreglo2[i].length; j++){
-                if (arreglo1 [i][j] != arreglo2 [i][j]){
+        for (int i = 0; i < arreglo2.length; i++) {
+            for (int j = 0; j < arreglo2[i].length; j++) {
+                if (arreglo1[i][j] != arreglo2[i][j]) {
                     System.out.println("NO TODOS LOS VALORES DE TODAS LAS CASILLAS DE AMBAS MATRICES SON IGUALES");
                     System.exit(0);
                 } else {
@@ -72,4 +68,67 @@ public class SPP404TeamNotFoundP2Problema2 {
         }
         System.out.println("LOS VALORES DE TODAS LAS CASILLAS DE AMBAS MATRICES SON IGUALES");
     }
+
+    public static int darTamaño(String dimension) {
+        int x;
+        boolean flag;
+        System.out.println("Valor de " + dimension);
+
+        do {
+            x = solicitaDatos();
+            flag = true;
+            if (x < 1) {
+                System.out.println("Introduzca un entero positivo(no hay longitudes negativas  o nulas de matrices)");
+                flag = false;
+            }
+
+        } while (flag == false);
+
+        return x;
+    }
+
+    public static int[][] darValores(int m, int n) {
+        int[][] matrix = new int[m][n];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.println("Introduzca valores para la posición " + i + ", " + j);
+                matrix[i][j] = solicitaDatos();
+            }
+        }
+
+        return matrix;
+    }
+
+    public static int solicitaDatos() {
+        Scanner teclado = new Scanner(System.in);
+        boolean flag;
+        int x = 0;
+        do {
+            try { 
+                x = teclado.nextInt();
+                flag = true;
+            } catch (Exception ex) { 
+                flag = false;
+                System.out.println("\nEl valor insertado es inválido. Introduzca un entero válido.\n" + ex + "\n");
+                teclado.nextLine();  
+            }
+
+        } while (flag == false);
+
+        return x; 
+    }
+
+    public static void muestraArreglo(int[][] matrix) { 
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print("[" + matrix[i][j] + "]");
+                if (j + 2 > matrix[i].length) {
+                    System.out.println("\t");
+                }
+            }
+        }
+
+    }
+
 }
